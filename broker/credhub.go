0bb806b93f76e5fd2f234e1c8f22ea367bbba10d
rkoster <rkoster@starkandwayne.com>
2021-03-03 16:46:39 +0100

    When binding an app grant credhub read permissions using mtls

--- broker/credhub.go
+++ broker/credhub.go
@@ -7,6 +7,7 @@ import (
 	"code.cloudfoundry.org/credhub-cli/credhub/auth"
 	"code.cloudfoundry.org/lager"
 	"github.com/starkandwayne/credhub-service-broker/config"
+
 	// "code.cloudfoundry.org/credhub-cli/credhub/credentials"
 	"code.cloudfoundry.org/credhub-cli/credhub/credentials/values"
 )
@@ -15,6 +16,8 @@ type CredHub interface {
 	WriteSecret(string, values.JSON) error
 	DeleteSecret(string) error
 	MakePath(string) string
+	AddReadPermission(instanceId, appGuid string) error
+	DeletePermission(instanceId, appGuid string) error
 }
 
 func NewCredHub(config config.Config, logger lager.Logger) (CredHub, error) {
@@ -41,6 +44,11 @@ type credhub struct {
 func (ch *credhub) MakePath(instanceId string) string {
 	return fmt.Sprintf("/%v/%v/%v/credentials", ch.config.ServiceName, ch.config.ServiceID, instanceId)
 }
+
+func MakeActor(appGuid string) string {
+	return fmt.Sprintf("mtls-app:%s", appGuid)
+}
+
 func (ch *credhub) WriteSecret(instanceId string, value values.JSON) error {
 	ch.logger.Info("Writing secret")
 	_, err := ch.client.SetJSON(ch.MakePath(instanceId), value)
@@ -50,4 +58,24 @@ func (ch *credhub) WriteSecret(instanceId string, value values.JSON) error {
 
 func (ch *credhub) DeleteSecret(instanceId string) error {
 	return ch.client.Delete(ch.MakePath(instanceId))
+}
+
+func (ch *credhub) AddReadPermission(instanceId, appGuid string) error {
+	_, err := ch.client.AddPermission(ch.MakePath(instanceId),
+		MakeActor(appGuid),
+		[]string{"read"},
+	)
+	return err
+}
+
+func (ch *credhub) DeletePermission(instanceId, appGuid string) error {
+	permission, err := ch.client.GetPermissionByPathActor(
+		ch.MakePath(instanceId), MakeActor(appGuid))
+	if err != nil {
+		return fmt.Errorf("failed to lookup permissions for instance: %s got: %s",
+			instanceId, err)
+	}
+
+	_, err = ch.client.DeletePermission(permission.UUID)
+	return err
 }
